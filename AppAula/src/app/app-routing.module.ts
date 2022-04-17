7d72f79a6a6f52c32d5f082e28c528097c8b9306
leGustta <gustavocarlos2108@gmail.com>
2022-04-16 22:23:58 -0400

    .

--- AppAula/src/app/app-routing.module.ts
+++ AppAula/src/app/app-routing.module.ts
@@ -10,6 +10,14 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'navegacao',
+    loadChildren: () => import('./AppAula/navegacao/navegacao.module').then( m => m.NavegacaoPageModule)
+  },
+  {
+    path: 'navegacao2',
+    loadChildren: () => import('./AppAula/navegacao2/navegacao2.module').then( m => m.Navegacao2PageModule)
   }
 ];
 
--- AppAula/src/app/app.component.ts
+++ AppAula/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
+
     { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
@@ -13,7 +14,11 @@ export class AppComponent {
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
     { title: 'Navegação', url: '/navegação', icon: 'code' },
+    { title: 'Notificação', url: '/folder/notificação', icon: 'bell' },
+    { title: 'Lembrete', url: '/lembrete', icon: 'question-mark' },
+    { title: 'Data', url: '/data', icon: 'Calendar' },
+    { title: 'Cima', url: '/cima ', icon: 'cloud' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Familia', 'Amigos', 'Notas', 'Texto'];
   constructor() {}
 }
